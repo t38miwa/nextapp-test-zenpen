d3363ec87668007fa4929cf63893185e13b146eb
t38miwa <t38miwa@gmail.com>
2024-05-08 12:43:35 +0900

    デプロイ後編

--- app/page.js
+++ app/page.js
@@ -1,95 +1,29 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+import Link from "next/link"
+import Image from "next/image"
 
-export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            DocS <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+const getAllItems = async() => {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/item/readall`, {cache: "no-store"})
+    const jsonData = await response.json()
+    const allItems = jsonData.allItems
+    return allItems
 }
+
+const ReadAllItems = async() => {
+    const allItems = await getAllItems()
+    return (
+        <div className="grid-container-in">
+            {allItems.map(item => 
+                <Link href={`/item/readsingle/${item._id}`} key={item._id}> 
+                    <Image src={item.image} width={750} height={500} alt="item-image" priority/>
+                    <div> 
+                        <h2>¥{item.price}</h2>
+                        <h3>{item.title}</h3>
+                        <p>{item.description.substring(0, 80)}...</p>  
+                    </div>
+                </Link>
+            )}
+        </div>
+    )
+} 
+
+export default ReadAllItems
--- middleware.js
+++ middleware.js
@@ -2,9 +2,7 @@ import { NextResponse } from "next/server"
 import { jwtVerify } from "jose"
 
 export async function middleware(request) {
-    const token = "eyJhbGciOiJIUzIdsfewwefwwfdtYWlsLmNvbSIsImV4cCI6MTcwMDIxMzk3MH0.tpJYWZgjbrR3gdR_5qA9hc4oo9k7-iYBsQGhy3zK2FU"
-    
-    //await request.headers.get("Authorization")?.split(" ")[1]
+    const token = await request.headers.get("Authorization")?.split(" ")[1]
     
     if(!token){
         return NextResponse.json({message: "トークンがありません"})
