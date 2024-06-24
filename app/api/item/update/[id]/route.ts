099bbf326898cc64ff59b6a5276b0f6586a0bb65
t38miwa <t38miwa@gmail.com>
2024-06-24 18:18:59 +0900

    開発完成３

--- app/api/item/update/[id]/route.ts
+++ app/api/item/update/[id]/route.ts
@@ -1,20 +1,22 @@
-import { NextResponse } from "next/server"
-import connectDB from "../../../../utils/database"
-import { ItemModel } from "../../../../utils/schemaModels"
+import { NextResponse } from "next/server";
+import connectDB from "../../../../utils/database";
+import { ItemModel } from "../../../../utils/schemaModels";
 
-export async function PUT(request: any, context: any) {
-    const reqBody = await request.json()
-    try{
-        await connectDB()
-        const singleItem = await ItemModel.findById(context.params.id)
-        // @ts-expect-error TS(2531): Object is possibly 'null'.
-        if(singleItem.email === reqBody.email){
-            await ItemModel.updateOne({_id: context.params.id}, reqBody)
-            return NextResponse.json({message: "アイテム編集成功"})
-        }else{
-            return NextResponse.json({message: "他の人が作成したアイテムです"})
+export async function PUT(request: Request, { params }: { params: { id: string } }) {
+    const reqBody = await request.json();
+    try {
+        await connectDB();
+        const singleItem = await ItemModel.findById(params.id);
+
+        if (singleItem && singleItem.email === reqBody.email) {
+            await ItemModel.updateOne({ _id: params.id }, reqBody);
+            return NextResponse.json({ message: "アイテム編集成功" });
+        } else if (!singleItem) {
+            return NextResponse.json({ message: "アイテムが見つかりません" });
+        } else {
+            return NextResponse.json({ message: "他の人が作成したアイテムです" });
         }
-    }catch(err){
-        return NextResponse.json({message: "アイテム編集失敗"})
+    } catch (err) {
+        return NextResponse.json({ message: "アイテム編集失敗" });
     }
 }
